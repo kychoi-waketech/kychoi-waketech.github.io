dd935874f178268fb9c3543d4949afdb60be6015
kychoi-waketech <kychoi2@my.waketech.edu>
2025-04-11 18:25:46 -0400

    Update gradebook.js

--- gradebook.js
+++ gradebook.js
@@ -1,1 +1,17 @@
+// TODO: Fetch data from the PostgreSQL database
+function fetchGradeData() {
+  //query PostgreSQL databse and return grade data
+  console.log("Fetching grade data...");
+}
 
+// TODO: Populate the table with grade data
+function populateGradebook(data) {
+  // This function will take the fetched grade data and populate the table
+  console.log("Populating gradebook with data:", data);
+}
+
+// TODO REMOVE THIS
+// Call the stubs to demonstrate the workflow
+const gradeData = fetchGradeData();
+populateGradebook(gradeData);
+//END REMOVE
