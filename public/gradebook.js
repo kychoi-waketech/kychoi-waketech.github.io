0d7985894bd4ccfb854aa62f20bb9c4635da3bdb
Mihchin <kychoi2@my.waketech.edu>
2025-05-03 14:21:46 -0400

    debugged

--- public/gradebook.js
+++ public/gradebook.js
@@ -9,11 +9,11 @@ function fetchGradeData() {
   // When the request changes status, we run this anonymous function
   xhr.onreadystatechange = function(){
     let results;
-    //check if we're done
-    if (xhr.readyState === xhr.DONE){
+    // Check if we're done
+    if(xhr.readyState === xhr.DONE){
       // Check if we're successful
-      if (xhr.status !== 200){
-        console.error(`Could not get grades. 
+      if(xhr.status !== 200){
+        console.error(`Could not get grades.
           Status: ${xhr.status}`);
       }
       // And then call the function to update the HTML with our data
@@ -28,12 +28,12 @@ function fetchGradeData() {
 function populateGradebook(data) {
   // This function will take the fetched grade data and populate the table
   console.log("Populating gradebook with data:", data);
-  let tableElm = document.getElementById("Gradebook"); //Get the gradebook table element
-    data.forEach(function(assignment){ // For each row of data we're passed in)
+  let tableElm = document.getElementById("gradebook"); // Get the gradebook table element
+    data.forEach(function(assignment){ // For each row of data we're passed in
       let row = document.createElement("tr") // create a table row element
-      let columns = []; //Handy place to stick the columns of information
-      columns.name = document.createElement('td'); //The first column's table data will be the name
-      column.name.appendChild(
+      let columns = []; // Handy place to stick the columns of information
+      columns.name = document.createElement('td'); // The first column's table data will be the name
+      columns.name.appendChild(
         // Concatenate the full name:  "last_name, first_name"
         document.createTextNode(assignment.last_name + ", " + assignment.first_name)
       );
